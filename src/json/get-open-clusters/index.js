24209cddae459e46d00825b20bc8eec994909ec4
Craig Ambrose <craig@craigambrose.com>
2018-07-15 14:30:07 +1000

    Returns clusters with too much data

--- src/json/get-open-clusters/index.js
+++ src/json/get-open-clusters/index.js
@@ -1,8 +1,28 @@
 const arc = require("@architect/functions")
+const data = require("@architect/data")
+
+function queryOpenClusters() {
+  return data.clusters.query({
+    KeyConditionExpression: "#isOpen = :isOpen",
+    IndexName: data._name("clusters-isOpen-index"),
+    ExpressionAttributeNames: {
+      "#isOpen": "isOpen"
+    },
+    ExpressionAttributeValues: {
+      ":isOpen": 1
+    }
+  })
+}
+
+async function allOpenClusters() {
+  const result = await queryOpenClusters()
+  return result.Items
+}
 
 async function route(req, res) {
+  const clusters = await allOpenClusters()
   res({
-    json: { msg: "posting membership" }
+    json: { clusters }
   })
 }
 
